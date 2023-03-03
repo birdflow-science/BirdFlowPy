2ab3dcc4eaaed23d1fc2983d3702fc844557166f
Miguel-Fuentes <Miguel.Fuentes.new@gmail.com>
2023-03-03 13:19:14 -0500

    added model hyperparameters as arguments to update_hdf.py

--- update_hdf.py
+++ update_hdf.py
@@ -19,19 +19,17 @@ parser = argparse.ArgumentParser(description='Run an amewoo model')
 parser.add_argument('root', type=str, help='hdf root directory')
 parser.add_argument('species', type=str, help='species name')
 parser.add_argument('resolution', type=int, help='model resolution')
+parser.add_argument('--obs_weight', help='Weight on the observation term of the loss', default=20.0, type=float)
+parser.add_argument('--dist_weight', help='Weight on the distance penalty in the loss', default=0.1, type=float)
+parser.add_argument('--ent_weight', help='Weight on the joint entropy of the model', default=0.02, type=float)
+parser.add_argument('--dist_pow', help='The exponent of the distance penalty', default=0.5, type=float)
+parser.add_argument('--learning_rate', help='Learning rate for Adam optimizer', default=0.1, type=float)
+parser.add_argument('--training_steps', help='The number of training iterations', default=1500, type=int)
+parser.add_argument('--rng_seed', help='Random number generator seed', default=17, type=int)
 
 args = parser.parse_args()
 
-print(args.root, args.species, args.resolution)
-
-rng_seed = 17
-training_steps = 1500
-
-learning_rate = 0.1
-obs_weight = 20.0
-ent_weight = 0.01
-dist_weight = 0.2
-dist_pow = 0.3
+print(str(args))
 
 file = h5py.File(os.path.join(args.root, f'{args.species}_2021_{args.resolution}km.hdf5'), 'r+')
 
@@ -47,25 +45,25 @@ nan_mask = ~np.asarray(file['geom']['mask']).flatten().astype(bool)
 dtuple = Datatuple(weeks, x_dim, y_dim, cells, nan_mask)
 
 d_matrix = gen_d_matrix(dtuple.x_dim, dtuple.y_dim, dtuple.nan_mask)
-d_matrix = d_matrix ** dist_pow
+d_matrix = d_matrix ** args.dist_pow
 
 # Get the random seed and optimizer
-key = hk.PRNGSequence(rng_seed)
-optimizer = optax.adam(learning_rate)
+key = hk.PRNGSequence(args.rng_seed)
+optimizer = optax.adam(args.learning_rate)
 
 # Instantiate loss function
 loss_fn = jit(partial(loss_fn, 
                       true_densities=true_densities, 
                       d_matrix=d_matrix, 
-                      obs_weight=obs_weight, 
-                      dist_weight=dist_weight,
-                      ent_weight=ent_weight))
+                      obs_weight=args.obs_weight, 
+                      dist_weight=args.dist_weight,
+                      ent_weight=args.ent_weight))
 
 
 # Run Training and get params and losses
 params, loss_dict = train_model(loss_fn,
                                 optimizer,
-                                training_steps,
+                                args.training_steps,
                                 dtuple.cells,
                                 dtuple.weeks,
                                 key)
